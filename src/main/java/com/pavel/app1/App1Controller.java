0c93ba1a8870264fa28464cd6f2f420be73448a8
Pavel <pavel.shendzeray@gmail.com>
2022-09-30 17:14:12 -0500

    init commit

---
+++ src/main/java/com/pavel/app1/App1Controller.java
@@ -0,0 +1,16 @@
+package com.pavel.app1;
+
+import com.pavel.module1.HelloService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class App1Controller {
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> callHello() {
+        String body = new HelloService().sayHello();
+        return ResponseEntity.ok(body);
+    }
+}
